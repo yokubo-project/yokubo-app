8bde68137934b6a20865d68895ae1267de9148bd
Markus Hoesel <markus.hoesel@allaboutapps.at>
2017-10-15 16:24:21 +0200

    Adding TabNavigation to Entries view

--- src/components/routes/Entries.tsx
+++ src/components/routes/Entries.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { StyleSheet, Text, View, ViewStyle, ScrollView } from "react-native";
-import { Header, List, ListItem } from "react-native-elements";
+import { StyleSheet, View, ViewStyle } from "react-native";
+import { Header, Icon } from "react-native-elements";
 import { Actions } from "react-native-router-flux";
-import * as _ from "lodash";
+import TabNavigator from "react-native-tab-navigator";
 
-import AddIcon from "../elements/AddIcon";
-import activities from "../../state/activities";
-
+import ListEntries from "./ListEntries";
 const primaryColor1 = "green";
 
 interface State {
+    selectedTab: string;
 }
 
 interface Props {
@@ -30,36 +29,10 @@ const styles = StyleSheet.create({
         backgroundColor: "#fff",
     } as ViewStyle,
     formContainer: {
-        flex: 2,
+        flexGrow: 6,
         justifyContent: "space-around",
         backgroundColor: "#fff",
     } as ViewStyle,
-    listContainer: {
-        flexGrow: 6,
-        // justifyContent: "space-around",
-        backgroundColor: "#fff",
-    } as ViewStyle,
-    button: {
-        position: "absolute",
-        bottom: 50,
-        right: 50,
-    },
-    tagContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "flex-start",
-        alignItems: "flex-start",
-    },
-    tagElement: {
-        backgroundColor: "gray",
-        marginLeft: 10,
-        marginTop: 10,
-        paddingLeft: 10,
-        paddingRight: 10,
-        paddingTop: 5,
-        paddingBottom: 5,
-        borderRadius: 3
-    }
 });
 
 @observer
@@ -67,82 +40,13 @@ export default class Component extends React.Component<Props, State> {
 
     constructor(props) {
         super(props);
+        this.state = {
+            selectedTab: "entryList",
+        };
     }
 
-    componentWillMount() {
-        activities.fetchEntries(this.props.uid);
-    }
-
-    handleOnIconClick() {
-        Actions.createEntry({
-            uid: this.props.uid,
-            inputMetrics: this.props.inputMetrics
-        });
-    }
-
-    sortEntries(sortKey, sortDirection) {
-        activities.sortEntries(sortKey, sortDirection);
-        // activities.sortEntries("metrices[0].metricValue", sortDirection);                
-    }
-
-    renderMetrices(entry) {
-        const metrices = entry.metrices.map(metric => {
-            return (
-                <Text key={metric.key}>{metric.metricName}: {metric.metricValue} {metric.metricUnity}</Text>
-            );
-        });
-
-        return (
-            <View>
-                <Text>Datum: {entry.datum}</Text>
-                {metrices}
-            </View>
-        );
-    }
-
-    renderMetricTags(entries) {
-
-        // TODO: Sort algorithmus not stable if entry is missing some metrics, as key may reference another metric
-        const metrices = [];
-        entries.forEach(entry => {
-            if (entry.metrices.length > 0) {
-                entry.metrices.forEach(metric => {
-                    metrices.push({
-                        metricName: metric.metricName,
-                        key: metric.key,
-                    });
-                });
-            }
-        });
-        const uniqMetricNames = _.uniqWith(metrices, _.isEqual);
-
-        const renderedMetricTagsAsc = uniqMetricNames.map(metric => {
-            return (
-                <Text
-                    key={`${metric.key}asc`}
-                    style={styles.tagElement}
-                    onPress={() => { this.sortEntries(`metrices[${metric.key}].metricValue`, "asc"); }}
-                >
-                    {`${metric.metricName} asc`}
-                </Text>
-            );
-        });
-
-        const renderedMetricTagsDesc = uniqMetricNames.map(metric => {
-            return (
-                <Text
-                    key={`${metric.key}desc`}
-                    style={styles.tagElement}
-                    onPress={() => { this.sortEntries(`metrices[${metric.key}].metricValue`, "desc"); }}
-                >
-                    {`${metric.metricName} desc`}
-                </Text>
-            );
-        });
-
-        const renderedMetricTags = renderedMetricTagsAsc.concat(renderedMetricTagsDesc);
-
-        return renderedMetricTags;
+    changeTab(selectedTab) {
+        this.setState({ selectedTab });
     }
 
     render() {
@@ -166,52 +70,35 @@ export default class Component extends React.Component<Props, State> {
                 </View>
 
                 <View style={styles.formContainer}>
-                    <View style={styles.tagContainer}>
-                        <Text
-                            style={styles.tagElement}
-                            onPress={() => { this.sortEntries("name", "asc"); }}
-                        >
-                            {"name asc"}
-                        </Text>
-                        <Text
-                            style={styles.tagElement}
-                            onPress={() => { this.sortEntries("name", "desc"); }}
-                        >
-                            {"name desc"}
-                        </Text>
-                        <Text
-                            style={styles.tagElement}
-                            onPress={() => { this.sortEntries("datum", "asc"); }}
-                        >
-                            {"datum asc"}
-                        </Text>
-                        <Text
-                            style={styles.tagElement}
-                            onPress={() => { this.sortEntries("datum", "desc"); }}
-                        >
-                            {"datum desc"}
-                        </Text>
-                        {this.renderMetricTags(activities.entries)}
-                    </View>
+                    <TabNavigator>
+                        <TabNavigator.Item
+                            titleStyle={{ fontWeight: "bold", fontSize: 10 }}
+                            selectedTitleStyle={{ marginTop: -1, marginBottom: 6, color: primaryColor1 }}
+                            selected={this.state.selectedTab === "entryList"}
+                            title={this.state.selectedTab === "entryList" ? "List" : null}
+                            renderIcon={() => <Icon containerStyle={{ justifyContent: "center", alignItems: "center", marginTop: 12 }} color={"#a9a9a9"} name="list" size={33} />}
+                            renderSelectedIcon={() => <Icon color={primaryColor1} name="list" size={30} />}
+                            onPress={() => this.changeTab("entryList")}>
+                            <ListEntries
+                                uid={this.props.uid}
+                                inputMetrics={this.props.inputMetrics}
+                            />
+                        </TabNavigator.Item>
+                        <TabNavigator.Item
+                            titleStyle={{ fontWeight: "bold", fontSize: 10 }}
+                            selectedTitleStyle={{ marginTop: -1, marginBottom: 6, color: primaryColor1 }}
+                            selected={this.state.selectedTab === "entryStats"}
+                            title={this.state.selectedTab === "entryStats" ? "Statistics" : null}
+                            renderIcon={() => <Icon containerStyle={{ justifyContent: "center", alignItems: "center", marginTop: 12 }} color={"#a9a9a9"} name="equalizer" size={33} />}
+                            renderSelectedIcon={() => <Icon color={primaryColor1} name="equalizer" size={30} />}
+                            onPress={() => this.changeTab("entryStats")}>
+                            <ListEntries
+                                uid={this.props.uid}
+                                inputMetrics={this.props.inputMetrics}
+                            />
+                        </TabNavigator.Item>
+                    </TabNavigator>
                 </View>
-
-                <ScrollView style={styles.listContainer}>
-                    <List containerStyle={{ marginBottom: 20 }}>
-                        {
-                            activities.entries.map((entry) => (
-                                <ListItem
-                                    key={entry.uid}
-                                    title={entry.name}
-                                    subtitle={this.renderMetrices(entry)}
-                                />
-                            ))
-                        }
-                    </List>
-                </ScrollView>
-
-                <AddIcon
-                    onPress={() => this.handleOnIconClick()}
-                />
 
             </View>
         );
---
+++ src/components/routes/ListEntries.tsx
@@ -0,0 +1,204 @@
+import React from "react";
+import { observer } from "mobx-react";
+import { StyleSheet, Text, View, ViewStyle, ScrollView } from "react-native";
+import { List, ListItem } from "react-native-elements";
+import { Actions } from "react-native-router-flux";
+import * as _ from "lodash";
+
+import AddIcon from "../elements/AddIcon";
+import activities from "../../state/activities";
+
+interface State {
+    selectedTab: string;
+}
+
+interface Props {
+    uid: string;
+    inputMetrics: any;
+}
+
+const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        // justifyContent: "space-around",
+        backgroundColor: "#fff",
+    } as ViewStyle,
+    formContainer: {
+        flex: 2,
+        justifyContent: "space-around",
+        backgroundColor: "#fff",
+    } as ViewStyle,
+    listContainer: {
+        flexGrow: 6,
+        // justifyContent: "space-around",
+        backgroundColor: "#fff",
+    } as ViewStyle,
+    button: {
+        position: "absolute",
+        bottom: 50,
+        right: 50,
+    },
+    tagContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+    },
+    tagElement: {
+        backgroundColor: "gray",
+        marginLeft: 10,
+        marginTop: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingTop: 5,
+        paddingBottom: 5,
+        borderRadius: 3
+    }
+});
+
+@observer
+export default class Component extends React.Component<Props, State> {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedTab: "profile",
+        };
+    }
+
+    changeTab(selectedTab) {
+        this.setState({ selectedTab });
+    }
+    
+    componentWillMount() {
+        activities.fetchEntries(this.props.uid);
+    }
+
+    handleOnIconClick() {
+        Actions.createEntry({
+            uid: this.props.uid,
+            inputMetrics: this.props.inputMetrics
+        });
+    }
+
+    sortEntries(sortKey, sortDirection) {
+        activities.sortEntries(sortKey, sortDirection);
+    }
+
+    renderMetrices(entry) {
+        const metrices = entry.metrices.map(metric => {
+            return (
+                <Text key={metric.key}>{metric.metricName}: {metric.metricValue} {metric.metricUnity}</Text>
+            );
+        });
+
+        return (
+            <View>
+                <Text>Datum: {entry.datum}</Text>
+                {metrices}
+            </View>
+        );
+    }
+
+    renderMetricTags(entries) {
+
+        // TODO: Sort algorithmus not stable if entry is missing some metrics, as key may reference another metric
+        const metrices = [];
+        entries.forEach(entry => {
+            if (entry.metrices.length > 0) {
+                entry.metrices.forEach(metric => {
+                    metrices.push({
+                        metricName: metric.metricName,
+                        key: metric.key,
+                    });
+                });
+            }
+        });
+        const uniqMetricNames = _.uniqWith(metrices, _.isEqual);
+
+        const renderedMetricTagsAsc = uniqMetricNames.map(metric => {
+            return (
+                <Text
+                    key={`${metric.key}asc`}
+                    style={styles.tagElement}
+                    onPress={() => { this.sortEntries(`metrices[${metric.key}].metricValue`, "asc"); }}
+                >
+                    {`${metric.metricName} asc`}
+                </Text>
+            );
+        });
+
+        const renderedMetricTagsDesc = uniqMetricNames.map(metric => {
+            return (
+                <Text
+                    key={`${metric.key}desc`}
+                    style={styles.tagElement}
+                    onPress={() => { this.sortEntries(`metrices[${metric.key}].metricValue`, "desc"); }}
+                >
+                    {`${metric.metricName} desc`}
+                </Text>
+            );
+        });
+
+        const renderedMetricTags = renderedMetricTagsAsc.concat(renderedMetricTagsDesc);
+
+        return renderedMetricTags;
+    }
+
+    render() {
+        return (
+            <View style={styles.mainContainer}>
+
+                <View style={styles.formContainer}>
+                    <View style={styles.tagContainer}>
+                        <Text
+                            style={styles.tagElement}
+                            onPress={() => { this.sortEntries("name", "asc"); }}
+                        >
+                            {"name asc"}
+                        </Text>
+                        <Text
+                            style={styles.tagElement}
+                            onPress={() => { this.sortEntries("name", "desc"); }}
+                        >
+                            {"name desc"}
+                        </Text>
+                        <Text
+                            style={styles.tagElement}
+                            onPress={() => { this.sortEntries("datum", "asc"); }}
+                        >
+                            {"datum asc"}
+                        </Text>
+                        <Text
+                            style={styles.tagElement}
+                            onPress={() => { this.sortEntries("datum", "desc"); }}
+                        >
+                            {"datum desc"}
+                        </Text>
+                        {this.renderMetricTags(activities.entries)}
+                    </View>
+                </View>
+
+                <ScrollView style={styles.listContainer}>
+                    <List containerStyle={{ marginBottom: 20 }}>
+                        {
+                            activities.entries.map((entry) => (
+                                <ListItem
+                                    key={entry.uid}
+                                    title={entry.name}
+                                    subtitle={this.renderMetrices(entry)}
+                                />
+                            ))
+                        }
+                    </List>
+                </ScrollView>
+
+                <AddIcon
+                    onPress={() => this.handleOnIconClick()}
+                />
+
+            </View>
+        );
+    }
+
+}
