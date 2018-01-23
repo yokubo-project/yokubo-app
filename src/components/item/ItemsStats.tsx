fc608cd2eb9abc5e3f9b006c6a9aecd86781d39e
Markus Hoesel <markus.hoesel@allaboutapps.at>
2018-01-23 22:17:54 +0100

    Styling and refactoring of ItemsStats component

--- src/components/item/ItemsStats.tsx
+++ src/components/item/ItemsStats.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { observer } from "mobx-react";
-import { StyleSheet, Text, View, ViewStyle } from "react-native";
+import { StyleSheet, Text, View, ViewStyle, TextStyle } from "react-native";
+import * as moment from "moment";
 
 import { IFullTask } from "../../state/taskStore";
+
+const backgroundColor = "#333333";
+const textColor = "#00F2D2";
+const errorTextColor = "#00F2D2";
+const inputTextColor = "#DDD";
 
 interface State {
     task: IFullTask;
@@ -15,13 +21,44 @@ interface Props {
 const styles = StyleSheet.create({
     mainContainer: {
         flex: 1,
-        backgroundColor: "#fff",
+        backgroundColor,
     } as ViewStyle,
     formContainer: {
         flex: 2,
         justifyContent: "space-around",
-        backgroundColor: "#fff",
+        backgroundColor,
     } as ViewStyle,
+    // metricTextEven: {
+    //     color: inputTextColor,
+    //     fontSize: 18,
+    //     paddingLeft: 10,
+    //     paddingTop: 5,
+    //     paddingBottom: 5,
+    //     marginLeft: 5,
+    //     marginRight: 5,
+    //     backgroundColor: "#4a4a4a"
+    // } as TextStyle,
+    // metricTextOdd: {
+    //     color: inputTextColor,
+    //     fontSize: 18,
+    //     paddingLeft: 10,
+    //     paddingTop: 5,
+    //     paddingBottom: 5,        
+    //     marginLeft: 5,
+    //     marginRight: 5,
+    //     backgroundColor
+    // } as TextStyle  
+    metricTextHeader: {
+        color: textColor,
+        fontSize: 18,
+        fontWeight: "bold",
+        marginLeft: 15
+    },
+    metricText: {
+        color: inputTextColor,
+        fontSize: 14,
+        marginLeft: 15
+    }
 });
 
 @observer
@@ -38,18 +75,46 @@ export default class Component extends React.Component<Props, State> {
     renderEntryStatictics(entries) {
 
         const metrices = [];
+        let duration: any = null;
         entries.forEach(entry => {
+
+            // Calculate duration
+            const start = moment.utc(entry.period[0]);
+            const end = moment.utc(entry.period[1]);
+            const ms = end.diff(start);
+            const days = moment.duration(ms);
+            const time = Math.floor(days.asHours()) + moment.utc(ms).format(":mm:ss");
+
+            if (duration !== null) {
+                duration.totalValue += ms;
+                duration.minValue = ms < duration.minValue ? ms : duration.minValue;
+                duration.maxValue = ms > duration.maxValue ? ms : duration.maxValue;
+            } else {
+                duration = {
+                    metricKey: "duration",
+                    metricName: "Duration",
+                    totalValue: ms,
+                    metricUnit: "ms",
+                    minValue: ms,
+                    maxValue: ms
+                };
+            }
+
             if (entry.metricQuantities.length > 0) {
                 entry.metricQuantities.forEach(metric => {
                     const metricObject = metrices.filter(metricOb => metricOb.metricName === metric.metric.name);
                     if (metricObject.length > 0) {
                         metricObject[0].totalValue += parseFloat(metric.quantity);
+                        metricObject[0].minValue = parseFloat(metric.quantity) < metricObject[0].minValue ? parseFloat(metric.quantity) : metricObject[0].minValue;
+                        metricObject[0].maxValue = parseFloat(metric.quantity) > metricObject[0].maxValue ? parseFloat(metric.quantity) : metricObject[0].maxValue;
                     } else {
                         metrices.push({
                             metricKey: metric.uid,
                             metricName: metric.metric.name,
                             totalValue: parseFloat(metric.quantity),
-                            metricUnity: metric.unit
+                            metricUnit: metric.metric.unit,
+                            minValue: parseFloat(metric.quantity),
+                            maxValue: parseFloat(metric.quantity),
                         });
                     }
                 });
@@ -58,13 +123,28 @@ export default class Component extends React.Component<Props, State> {
 
         const renderedMetrices = metrices.map(metric => (
             <View key={metric.metricKey}>
-                <Text>Metricname: {metric.metricName}</Text>
-                <Text>Total: {metric.totalValue} {metric.metricUnity}</Text>
-                <Text>Average: {metric.totalValue / entries.length} {metric.metricUnity}</Text>
+                <Text style={styles.metricTextHeader}>{metric.metricName}</Text>
+                <Text style={styles.metricText}>Total: {metric.totalValue} {metric.metricUnit}</Text>
+                <Text style={styles.metricText}>Average: {(metric.totalValue / entries.length).toFixed(2)} {metric.metricUnit}</Text>
+                <Text style={styles.metricText}>Min:  {metric.minValue} {metric.metricUnit}</Text>
+                <Text style={styles.metricText}>Max:  {metric.maxValue} {metric.metricUnit}</Text>
             </View>
         ));
 
-        renderedMetrices.push(<View key={"count"}><Text>Metricname: Einheiten</Text><Text>Total: {entries.length}</Text></View>);
+        renderedMetrices.unshift(
+            <View key={"duration"}>
+                <Text style={styles.metricTextHeader}>{duration.metricName}</Text>
+                <Text style={styles.metricText}>Total: {Math.floor(moment.duration(duration.totalValue).asHours()) + moment.utc(duration.totalValue).format(":mm:ss")}</Text>
+                <Text style={styles.metricText}>Average: {Math.floor(moment.duration(duration.totalValue / entries.length).asHours()) + moment.utc(duration.totalValue / entries.length).format(":mm:ss")}</Text>
+                <Text style={styles.metricText}>Min:  {Math.floor(moment.duration(duration.minValue).asHours()) + moment.utc(duration.minValue).format(":mm:ss")}</Text>
+                <Text style={styles.metricText}>Max:  {Math.floor(moment.duration(duration.maxValue).asHours()) + moment.utc(duration.maxValue).format(":mm:ss")}</Text>
+            </View>);
+
+        renderedMetrices.unshift(
+            <View key={"count"}>
+                <Text style={styles.metricTextHeader}>Einheiten</Text>
+                <Text style={styles.metricText}>Total: {entries.length}</Text>
+            </View>);
 
         return renderedMetrices;
     }
